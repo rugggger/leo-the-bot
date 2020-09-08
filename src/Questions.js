1bdd1820d8621b3dda13709220b27368bda97b49
Yaron Bar <rugggger@yahoo.co.uk>
2020-09-08 18:58:33 +0300

    questions

---
+++ src/Questions.js
@@ -0,0 +1,14 @@
+const questions = [
+  {
+    category: "Segments",
+    question: "Can I use segements from Google Analytics ?",
+    answer:
+      "Yes. You need an integration. You can talk about it with your CSM.",
+    actions: [
+      {
+        type: "openSupportTicket",
+        params: { message: "Integrate with Google Analytics" },
+      },
+    ],
+  },
+];
