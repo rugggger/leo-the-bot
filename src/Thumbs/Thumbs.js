a3858702d225797b4050f95b2d3330fbf781dad7
Yaron Bar <rugggger@yahoo.co.uk>
2020-09-08 17:32:42 +0300

    added material ui

--- src/Thumbs/Thumbs.js
+++ src/Thumbs/Thumbs.js
@@ -1,22 +1,32 @@
 import React from "react";
 import Emoji from "../misc/Emoji";
+import Button from "@material-ui/core/Button";
 
 const Thumbs = (props) => {
   const { messages, actionProvider } = props;
   const { handleThumbsDown } = actionProvider;
-  const getLastMessage = ()=>{
-    return messages[messages.length-2].message
-  }
+  const getLastMessage = () => {
+    return messages[messages.length - 2].message;
+  };
   const lastMessage = getLastMessage();
   return (
-      <div>
-          <button
-          onClick={()=>{console.log('clicked ', props); }}
-          ><Emoji emoji={"thumbsup"}/></button>
-          <button
-            onClick={()=>{handleThumbsDown(lastMessage)}}
-          ><Emoji emoji={"thumbsdown"}/></button>
-      </div>
+    <div>
+      <Button
+        onClick={() => {
+          console.log("clicked ", props);
+        }}
+      >
+        <Emoji emoji={"thumbsup"} />
+      </Button>
+
+      <Button
+        onClick={() => {
+          handleThumbsDown(lastMessage);
+        }}
+      >
+        <Emoji emoji={"thumbsdown"} />
+      </Button>
+    </div>
   );
 };
 
